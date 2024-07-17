868d3d780c39a0047c7c5acb3c20f3642b974422
miguelruizc <ruizconde.m@gmail.com>
2024-07-17 12:49:38 +0200

    Populate message board with posts from database

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('node:path');
+const mongoose = require('mongoose');
+const Post = require('./models/Post');
 
 const port = process.env.PORT || 3000;
 
@@ -22,8 +24,18 @@ app.get('/post', (req, res, next) => {
 app.get('/logout', (req, res, next) => {
 	res.redirect('/');
 });
-app.get('/', (req, res, next) => {
-	res.status(200).render('index', { title: 'Only Members' });
+app.get('/', async (req, res, next) => {
+	const posts = await Post.find({});
+	res.status(200).render('index', { title: 'Only Members', posts });
 });
 
-app.listen(port, () => console.log(`Server running, listening port ${port}`));
+mongoose
+	.connect(process.env.MONGODB_URI)
+	.then(() => {
+		console.log('Conected to database: ', mongoose.connection.name);
+
+		app.listen(port, () =>
+			console.log(`Server running, listening port ${port}`)
+		);
+	})
+	.catch((err) => console.error('Error connecting to database: ', err));
