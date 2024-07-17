ede681053f6a6a49596aa5763a44b098506ab63d
miguelruizc <ruizconde.m@gmail.com>
2024-07-17 12:16:18 +0200

    Add first html view

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('node:path');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
 app.get('/', (req, res, next) => {
-	res.send('Hello!\n');
+	res.status(200).render('index', { title: 'Only Members' });
 });
 
 app.listen(port, () => console.log(`Server running, listening port ${port}`));
