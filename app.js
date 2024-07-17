b2f045219f9ec3f34ee0d6d480dca8112f3b1335
miguelruizc <ruizconde.m@gmail.com>
2024-07-17 16:26:52 +0200

    Change signup to register and clean up

--- app.js
+++ app.js
@@ -2,10 +2,35 @@ const express = require('express');
 const path = require('node:path');
 const mongoose = require('mongoose');
 const Post = require('./models/Post');
+// const passport = require('passport');
+// const session = require('express-session');
+// const LocalStrategy = require('passport-local').Strategy;
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+// // Middleware for passport
+// app.use(express.urlencoded({ extended: true }));
+// app.use(session({ secret: process.env.SECRET }));
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// // Passport configuration
+// passport.use(
+// 	newLocalStrategy((username, password, done) => {
+// 		const user = users.find(
+// 			(u) => u.username === username && u.password === password
+// 		);
+// 		if (!user) return done(null, false, { message: 'Invalid credentials' });
+// 		return done(null, user);
+// 	})
+// );
+// passport.serializeUser((user, done) => done(null, user.id));
+// passport.deserializeUser((id, done) => {
+// 	const user = users.find((u) => u.id === id);
+// 	done(null, user);
+// });
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -15,8 +40,8 @@ app.use(express.static('public'));
 app.get('/login', (req, res, next) => {
 	res.status(200).render('login', { title: 'Log In' });
 });
-app.get('/signup', (req, res, next) => {
-	res.status(200).render('signup', { title: 'Sign Up' });
+app.get('/register', (req, res, next) => {
+	res.status(200).render('register', { title: 'Register' });
 });
 app.get('/post', (req, res, next) => {
 	res.status(200).render('post', { title: 'Log In' });
